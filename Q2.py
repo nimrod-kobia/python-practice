974476d4e550c366516184b1e4cd3fa933327708
nimrod-kobia <nimrod.kobia@strathmore.edu>
2025-04-25 12:15:01 +0300

    Update Q2.py
    Created an input function then converted the  input to an integer and divided it by 2 to see if its an even or odd number

--- Q2.py
+++ Q2.py
@@ -1,1 +1,7 @@
+print("Kindly input a number") #ask the user to input a number
+number = int(input()) #convert their input into an integer
 
+if number % 2 == 0: #even numbers can be divided by 2 and leave no remainders
+    print("This is an even number")
+else:
+    print("This is an odd number")
