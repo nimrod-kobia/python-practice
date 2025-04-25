a701e3d3a88e74be015aa5d11eeb74911b76c0c0
nimrod-kobia <nimrod.kobia@strathmore.edu>
2025-04-25 11:51:50 +0300

    Update Q1.py
    Added the function and output the answers

--- Q1.py
+++ Q1.py
@@ -1,3 +1,7 @@
-#start by creating a list of numbers and assigning it the name 'sum'
-sum=[1,2,3,4,5,6]
+#start by creating a list of numbers and assigning it the name ' numbers'
+numbers=[1,2,3,4,5,6]
+##assigned the function sum and named it sum to add the numbers in the list
+sum=sum(numbers)
+##print the value from the function
+print(sum)
 
