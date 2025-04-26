b9b4d0efe7bdfb85b032a320ead8a5102d948415
nimrod-kobia <nimrod.kobia@strathmore.edu>
2025-04-26 12:25:30 +0300

    Update Q6.py
    Added the modulus and truncated division to ensure the proper numbers are obtained from calculations

--- Q6.py
+++ Q6.py
@@ -1,1 +1,11 @@
+num = int(input("Input your integer value :")) #collects input from user
+result = 0
+
+while num > 0: #ensures the loop ends once 0 is obtained
+    digit = num % 10 #modulus used to get the remainder of a number
+    result = result + digit #the remainder is then added to the previous stored number
+    num = num // 10 #this ensures the number added has been removed from the initial value and uses the truncated division for a rounded off number
+
+print("Result is: ", result) #outputs the answer obtained
+
 
